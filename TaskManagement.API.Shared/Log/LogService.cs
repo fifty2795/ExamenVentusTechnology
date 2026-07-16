a23ded1aa588211b251afe0c7693f39097f2de19
Alejandro Arizmendi <alejandro_arizmendi@hotmail.es>
2026-07-16 05:19:02 -0600

    Se agrega carpeta Logger

---
+++ TaskManagement.API.Shared/Log/LogService.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.API.Shared.Interfaces;
+using TaskManagement.API.Shared.Settings;
+
+namespace TaskManagement.API.Shared.Log
+{   
+    public class LogService : ILogService
+    {
+        private readonly LogSettings _logSettings;
+
+        public LogService(IOptions<LogSettings> logSettings)
+        {
+            _logSettings = logSettings.Value;
+        }
+
+        public void LogError(string message, Exception? ex = null)
+        {
+            Log("ERROR", message, ex);
+        }
+
+        public void LogInfo(string message)
+        {
+            Log("INFO", message);
+        }
+
+        public void LogWarning(string message)
+        {
+            Log("WARNING", message);
+        }
+
+        private void Log(string level, string message, Exception? ex = null)
+        {
+            var logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{level}] {message}";
+
+            string directory = _logSettings.LogFilePath;
+            string logPath = Path.Combine(directory, _logSettings.LogFileName);
+
+            if (ex != null)
+            {
+                logMessage += Environment.NewLine +
+                              $"Exception: {ex.Message}" +
+                              Environment.NewLine +
+                              $"StackTrace: {ex.StackTrace}";
+            }
+
+            try
+            {
+                // Crear la carpeta si no existe
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                // Rotar el log si alcanzó el tamaño máximo
+                if (File.Exists(logPath) &&
+                    new FileInfo(logPath).Length >= _logSettings.TamanioMaximo)
+                {
+                    RotateLogs();
+                }
+
+                File.AppendAllText(
+                    logPath,
+                    Environment.NewLine + logMessage + Environment.NewLine);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error al escribir en Log: {e.Message}");
+            }
+        }
+
+        private void RotateLogs()
+        {
+            try
+            {
+                string logPath = _logSettings.LogFilePath + "\\" + _logSettings.LogFileName;
+                var backupFilePath = Path.Combine(_logSettings.LogFilePath, $"Log_{DateTime.UtcNow:yyyyMMddHHmmss}.txt");
+                File.Move(logPath, backupFilePath);
+
+                // Eliminar archivos de respaldo más antiguos si el número máximo se excede                
+                var files = Directory.GetFiles(_logSettings.LogFilePath, "Log_*.txt")
+                      .OrderByDescending(f => File.GetCreationTime(f))
+                      .Skip(_logSettings.MaximoArchivosLog)
+                      .ToList();
+
+
+                foreach (var file in files)
+                {
+                    File.Delete(file);
+                }
+
+                // Crear un nuevo archivo de log vacío
+                File.Create(logPath).Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error en la rotación de logs: {e.Message}");
+            }
+        }
+    }
+}
